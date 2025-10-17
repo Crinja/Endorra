6b622d2e7cac4d957f488a8fa675b3020f813a26
Crinja <125954850+Crinja@users.noreply.github.com>
2025-10-17 21:14:47 +1100

    .

---
+++ catalog/src/test/java/com/techtech/endorra/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtech.endorra.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
