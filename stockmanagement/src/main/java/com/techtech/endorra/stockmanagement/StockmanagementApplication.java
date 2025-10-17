c6de9d23eb43bc1d684dc83be5cdb404c14af06c
Crinja <125954850+Crinja@users.noreply.github.com>
2025-10-17 18:00:40 +1100

    Added RestTemplate

--- stockmanagement/src/main/java/com/techtech/endorra/stockmanagement/StockmanagementApplication.java
+++ stockmanagement/src/main/java/com/techtech/endorra/stockmanagement/StockmanagementApplication.java
@@ -2,6 +2,9 @@ package com.techtech.endorra.stockmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class StockmanagementApplication {
@@ -10,4 +13,8 @@ public class StockmanagementApplication {
 		SpringApplication.run(StockmanagementApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
