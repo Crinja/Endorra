cc70c08379614efb7c6e4a5185d96737dc6fbeb6
Crinja <125954850+Crinja@users.noreply.github.com>
2025-10-19 17:45:50 +1100

    Added find by tag

--- stockmanagement/src/main/java/com/techtech/endorra/stockmanagement/repository/ProductRepository.java
+++ stockmanagement/src/main/java/com/techtech/endorra/stockmanagement/repository/ProductRepository.java
@@ -1,10 +1,20 @@
 package com.techtech.endorra.stockmanagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.techtech.endorra.stockmanagement.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> 
 {
-    
+    @Query("""
+        SELECT DISTINCT p 
+        FROM Product p 
+        JOIN p.tags t 
+        WHERE LOWER(t) IN :tags
+    """)
+    List<Product> findByAnyTags(@Param("tags") List<String> tags);
 }
